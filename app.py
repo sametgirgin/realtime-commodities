172212b7fc50c35504e65167ed6439c674dcd3af
SAMET GİRGİN <44918645+sametgirgin@users.noreply.github.com>
2025-02-27 14:30:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -102,16 +102,6 @@ if selected_commodity in COMMODITY_INFO:
     with tabs[0]:
         st.markdown("### Description")
         st.write(info["Description"])
-        
-    with tabs[1]:
-        col1, col2 = st.columns(2)
-        with col1:
-            st.markdown("### Trading Hours")
-            st.write(info["trading_hours"])
-            st.markdown("### Trade")
-            st.write(info["Trade"])
-            st.markdown("### Markets")
-            st.write(info["Market"])
 
 # Add a divider
 st.markdown("---")
